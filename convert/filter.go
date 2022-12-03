1d2765c805c558cd38f7b79cff9abb0561fad50f
Minh Tâm <minhtam30102002@gmail.com>
2022-12-03 15:30:31 +0700

    Final

--- convert/filter.go
+++ convert/filter.go
@@ -1,7 +1,6 @@
 package convert
 
 import (
-	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
@@ -37,7 +36,6 @@ func SumTotal(arr2d [][]string) (total int) {
 	for i := 0; i < len(arr2d); i++ {
 		getInt, err := strconv.Atoi(arr2d[i][4])
 		if err != nil {
-			fmt.Errorf("Error while parsing: %", err)
 			return
 		}
 		total += getInt
